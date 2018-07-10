7c741c4f1c365dd0345e3888cfe151982e7ca316
Daniel Voicu <daniel.voicu@brandfusion.ro>
2018-07-11 00:31:54 +0300

    first commit

---
+++ checkout.js
@@ -0,0 +1,549 @@
+let countries = [  
+  {
+    "country": "Australia",
+    "countryCode": "AUS",
+    "regions": []
+  },
+  {
+    "country": "Canada",
+    "countryCode": "CA",
+    "regions": [
+      {
+      "name": "Alberta",
+      "code": "AB"
+      },
+      {
+      "name": "British Columbia",
+      "code": "BC"
+      },
+      {
+      "name": "Manitoba",
+      "code": "MB"
+      },
+      {
+      "name": "New Brunswick",
+      "code": "NB"
+      },
+      {
+      "name": "Newfoundland and Labrador",
+      "code": "NL"
+      },
+      {
+      "name": "Northwest Territories",
+      "code": "NT"
+      },
+      {
+      "name": "Nova Scotia",
+      "code": "NS"
+      },
+      {
+      "name": "Nunavut",
+      "code": "NU"
+      },
+      {
+      "name": "Ontario",
+      "code": "ON"
+      },
+      {
+      "name": "Prince Edward Island",
+      "code": "PE"
+      },
+      {
+      "name": "Quebec",
+      "code": "QC"
+      },
+      {
+      "name": "Saskatchewan",
+      "code": "SK"
+      },
+      {
+      "name": "Yukon Territory",
+      "code": "YT"
+      }
+      ]
+  },
+  {
+    "country": "United States of America",
+    "countryCode": "US",
+    "regions": [
+      {
+      "name": "Alabama",
+      "code": "AL"
+      },
+      {
+      "name": "Alaska",
+      "code": "AK"
+      },
+      {
+      "name": "American Samoa",
+      "code": "AS"
+      },
+      {
+      "name": "Arizona",
+      "code": "AZ"
+      },
+      {
+      "name": "Arkansas",
+      "code": "AR"
+      },
+      {
+      "name": "California",
+      "code": "CA"
+      },
+      {
+      "name": "Colorado",
+      "code": "CO"
+      },
+      {
+      "name": "Connecticut",
+      "code": "CT"
+      },
+      {
+      "name": "Delaware",
+      "code": "DE"
+      },
+      {
+      "name": "District Of Columbia",
+      "code": "DC"
+      },
+      {
+      "name": "Federated States Of Micronesia",
+      "code": "FM"
+      },
+      {
+      "name": "Florida",
+      "code": "FL"
+      },
+      {
+      "name": "Georgia",
+      "code": "GA"
+      },
+      {
+      "name": "Guam",
+      "code": "GU"
+      },
+      {
+      "name": "Hawaii",
+      "code": "HI"
+      },
+      {
+      "name": "Idaho",
+      "code": "ID"
+      },
+      {
+      "name": "Illinois",
+      "code": "IL"
+      },
+      {
+      "name": "Indiana",
+      "code": "IN"
+      },
+      {
+      "name": "Iowa",
+      "code": "IA"
+      },
+      {
+      "name": "Kansas",
+      "code": "KS"
+      },
+      {
+      "name": "Kentucky",
+      "code": "KY"
+      },
+      {
+      "name": "Louisiana",
+      "code": "LA"
+      },
+      {
+      "name": "Maine",
+      "code": "ME"
+      },
+      {
+      "name": "Marshall Islands",
+      "code": "MH"
+      },
+      {
+      "name": "Maryland",
+      "code": "MD"
+      },
+      {
+      "name": "Massachusetts",
+      "code": "MA"
+      },
+      {
+      "name": "Michigan",
+      "code": "MI"
+      },
+      {
+      "name": "Minnesota",
+      "code": "MN"
+      },
+      {
+      "name": "Mississippi",
+      "code": "MS"
+      },
+      {
+      "name": "Missouri",
+      "code": "MO"
+      },
+      {
+      "name": "Montana",
+      "code": "MT"
+      },
+      {
+      "name": "Nebraska",
+      "code": "NE"
+      },
+      {
+      "name": "Nevada",
+      "code": "NV"
+      },
+      {
+      "name": "New Hampshire",
+      "code": "NH"
+      },
+      {
+      "name": "New Jersey",
+      "code": "NJ"
+      },
+      {
+      "name": "New Mexico",
+      "code": "NM"
+      },
+      {
+      "name": "New York",
+      "code": "NY"
+      },
+      {
+      "name": "North Carolina",
+      "code": "NC"
+      },
+      {
+      "name": "North Dakota",
+      "code": "ND"
+      },
+      {
+      "name": "Northern Mariana Islands",
+      "code": "MP"
+      },
+      {
+      "name": "Ohio",
+      "code": "OH"
+      },
+      {
+      "name": "Oklahoma",
+      "code": "OK"
+      },
+      {
+      "name": "Oregon",
+      "code": "OR"
+      },
+      {
+      "name": "Palau",
+      "code": "PW"
+      },
+      {
+      "name": "Pennsylvania",
+      "code": "PA"
+      },
+      {
+      "name": "Puerto Rico",
+      "code": "PR"
+      },
+      {
+      "name": "Rhode Island",
+      "code": "RI"
+      },
+      {
+      "name": "South Carolina",
+      "code": "SC"
+      },
+      {
+      "name": "South Dakota",
+      "code": "SD"
+      },
+      {
+      "name": "Tennessee",
+      "code": "TN"
+      },
+      {
+      "name": "Texas",
+      "code": "TX"
+      },
+      {
+      "name": "Utah",
+      "code": "UT"
+      },
+      {
+      "name": "Vermont",
+      "code": "VT"
+      },
+      {
+      "name": "Virgin Islands",
+      "code": "VI"
+      },
+      {
+      "name": "Virginia",
+      "code": "VA"
+      },
+      {
+      "name": "Washington",
+      "code": "WA"
+      },
+      {
+      "name": "West Virginia",
+      "code": "WV"
+      },
+      {
+      "name": "Wisconsin",
+      "code": "WI"
+      },
+      {
+      "name": "Wyoming",
+      "code": "WY"
+      }
+    ]
+  }
+
+]
+let addresses = [
+  {"id": 1, 
+  "EcomOrderCustomerName": "Customer1",
+  "EcomOrderCustomerPhone": "555-555-5555",
+  "EcomOrderCustomerEmail": "Customer1@test.com",
+  "EcomOrderCustomerCompany": "Company1",
+  "EcomOrderCustomerAddress": "Address1",
+  "EcomOrderCustomerAddress2": "1",
+  "EcomOrderCustomerCity": "Washington",  
+  "EcomOrderCustomerCountry": "US",
+  "EcomOrderCustomerRegion": "WA",
+  "EcomOrderCustomerZip": "Zip1",
+  },
+  {"id": 2, 
+  "EcomOrderCustomerName": "Customer2",
+  "EcomOrderCustomerPhone": "555-555-5555",
+  "EcomOrderCustomerEmail": "Customer2@test.com",
+  "EcomOrderCustomerCompany": "Company2",
+  "EcomOrderCustomerAddress": "Address2",
+  "EcomOrderCustomerAddress2": "2",
+  "EcomOrderCustomerCity": "Seattle",  
+  "EcomOrderCustomerCountry": "US",
+  "EcomOrderCustomerRegion": "WA",
+  "EcomOrderCustomerZip": "Zip2",
+  },
+  {"id": 3, 
+  "EcomOrderCustomerName": "Customer3",
+  "EcomOrderCustomerPhone": "555-555-5555",
+  "EcomOrderCustomerEmail": "Customer3@test.com",
+  "EcomOrderCustomerCompany": "Company3",
+  "EcomOrderCustomerAddress": "Address3",
+  "EcomOrderCustomerAddress2": "3",
+  "EcomOrderCustomerCity": "Los Angeles",  
+  "EcomOrderCustomerCountry": "US",
+  "EcomOrderCustomerRegion": "CA",
+  "EcomOrderCustomerZip": "Zip3",
+  },
+  {"id": 4, 
+  "EcomOrderCustomerName": "Customer4",
+  "EcomOrderCustomerPhone": "555-555-5555",
+  "EcomOrderCustomerEmail": "Customer4@test.com",
+  "EcomOrderCustomerCompany": "Company4",
+  "EcomOrderCustomerAddress": "Address4",
+  "EcomOrderCustomerAddress2": "4",
+  "EcomOrderCustomerCity": "Austin",
+  "EcomOrderCustomerCountry": "US",
+  "EcomOrderCustomerRegion": "TX",  
+  "EcomOrderCustomerZip": "Zip4",
+  }
+]
+const {createStore} = Redux;
+
+const reducer = (state, {type,payload}) => { 
+  if (type === "ChangeBillingCheckbox") {
+    return payload ? {...state, "billingAddress": {...state.billingAddress, "selected": state.billingAddress.default}} : {...state, "billingAddress": {...state.billingAddress, "selected": null}, billingIsShipping: payload}
+  } 
+  if (type === "ChangeDeliveryEditCheckbox") {    
+    return payload ? {...state, "shippingAddress": {...state.shippingAddress, "selected": null}} : {...state, "shippingAddress": {...state.shippingAddress, "selected": state.shippingAddress.default}}
+  } 
+  if (type === "UpdateDeliveryAddress") {    
+    return {...state, "shippingAddress": {"default": payload, "selected": payload}, "billingAddress": {"default": payload, "selected": state.billingAddress.selected !== null ? payload : null}}
+  } 
+  return state;
+}
+
+const state = {
+  "shippingAddress": {
+    "default": 1,
+    "selected": 1
+  },
+  "billingAddress": {
+    "default": 1,
+    "selected": 1
+  },
+  "addresses": addresses,
+  "billingIsShipping": true,
+  "countries": countries
+}
+
+const store = createStore(reducer, state);
+store.subscribe(() => {
+  console.log("store changed to:  ", store.getState());
+});
+
+//Functions
+const getDataJSON = (country) => {  
+  let dataJson = store.getState().countries;
+  return new Promise((resolve,reject) => {
+    if (country == undefined) {
+      resolve(dataJson);
+    } else {
+      resolve(dataJson.filter(v => v.countryCode === country));
+    }
+    
+  });
+}
+const featureCountry = (data,val) => {
+  return [...data.filter(o => val.includes(o.countryCode)),...data.filter(o => !(val.includes(o.countryCode)))];  
+}
+const outputCountries = (data) => {
+  return new Promise((resolve,reject) => { 
+    let d = data.reduce((r,v,k) => {
+      return r + '<option value='+v.countryCode+'>'+v.country+'</option>'
+    },"");
+    document.querySelectorAll("select.country").forEach(n => {
+      n.innerHTML = d;
+    })
+    resolve(data);
+  })
+}
+const outputRegions = (data, node = undefined) => {
+  return new Promise((resolve,reject)=>{   
+    if (node == undefined) {
+      document.querySelectorAll("select.region").forEach(n => {
+        n.innerHTML = data[0].regions.reduce((r,v,k) => {
+          return r + '<option value='+v.code+'>'+v.name+'</option>'
+        },"");
+      });
+    } else {
+      node.innerHTML = data[0].regions.reduce((r,v,k) => {
+        return r + '<option value='+v.code+'>'+v.name+'</option>'
+      },"");
+    }
+    
+    resolve(data[0]);
+  });
+}
+const copyDeliveryStateAndRegion = () => {
+  let country = document.getElementById("EcomOrderDeliveryCountry").value;
+  let state = document.getElementById("EcomOrderDeliveryRegion").value;
+  
+  getDataJSON(country)
+    .then((data) => {
+      document.getElementById("EcomOrderCustomerCountry").value = country;      
+      return new Promise((resolve, reject)=>{
+        resolve(data);
+      });
+    })
+    .then((data)=>{return outputRegions(data, document.getElementById("EcomOrderCustomerRegion"))})
+    .then((data) => {
+      document.getElementById("EcomOrderCustomerRegion").value = state;
+      return new Promise((resolve, reject)=>{
+        resolve(data);
+      });
+    })
+}
+const fillAddress = (area = "Customer") => {
+  let id = area === "Customer" ? store.getState().billingAddress.default : store.getState().shippingAddress.default;
+  let address = store.getState().addresses.filter(o=>o.id === id)[0];  
+    for (let key in address) {
+      if (address.hasOwnProperty(key)) {        
+        if (key.includes("Customer")) {                 
+          if(key.includes("Region") === true) {            
+            getDataJSON(address["EcomOrderCustomerCountry"])
+              .then((data)=>{
+                if (area === "Customer") {
+                  return outputRegions(data, document.getElementById("EcomOrderCustomerRegion"))
+                } else {
+                  return outputRegions(data, document.getElementById("EcomOrderDeliveryRegion"))
+                }
+              })
+              .then((data)=>{   
+                if (area === "Customer") {
+                  document.getElementById(key).value = address[key];
+                } else {
+                  document.getElementById(key.replace("Customer","Delivery")).value = address[key];
+                }
+                
+              }) 
+          } else {
+            if (area === "Customer") {
+              document.getElementById(key).value = address[key];
+            } else {
+              document.getElementById(key.replace("Customer","Delivery")).value = address[key];
+            }      
+          }      
+          
+        }
+      }
+    }
+}
+
+const getCountryText = (code) => {
+  return store.getState().countries.filter(o => o.countryCode === code)[0].country;
+}
+const renderDeliveryAddressReadonly = (id) => { 
+  let address = store.getState().addresses.filter(o=>o.id === id)[0];  
+  document.getElementById("shipping-address-readonly").innerHTML = `<p>${address.EcomOrderCustomerAddress}<br>${address.EcomOrderCustomerCity}, ${address.EcomOrderCustomerRegion} ${address.EcomOrderCustomerZip}<br>${getCountryText(address.EcomOrderCustomerCountry)}<br>${address.EcomOrderCustomerEmail}<br>${address.EcomOrderCustomerPhone}</p>`;
+  document.getElementById("summary-shipping").innerHTML = `<p>${address.EcomOrderCustomerAddress}<br>${address.EcomOrderCustomerCity}, ${address.EcomOrderCustomerRegion} ${address.EcomOrderCustomerZip}<br>${getCountryText(address.EcomOrderCustomerCountry)}<br>${address.EcomOrderCustomerEmail}<br>${address.EcomOrderCustomerPhone}</p>`;
+  if (store.getState().billingIsShipping) {
+    document.getElementById("summary-billing").innerHTML = `<p>${address.EcomOrderCustomerAddress}<br>${address.EcomOrderCustomerCity}, ${address.EcomOrderCustomerRegion} ${address.EcomOrderCustomerZip}<br>${getCountryText(address.EcomOrderCustomerCountry)}<br>${address.EcomOrderCustomerEmail}<br>${address.EcomOrderCustomerPhone}</p>`;
+  }
+}
+
+//Events
+document.getElementById("editShippingAddress").querySelector("input").addEventListener('change',(e) => {  
+  store.dispatch({type: "ChangeDeliveryEditCheckbox", payload: e.currentTarget.checked});  
+  if (e.currentTarget.checked) {
+    fillAddress("Delivery");
+    document.getElementById("shipping-address").classList.remove("d-none");
+    document.getElementById("shipping-address-readonly").classList.add("d-none");
+    document.getElementById("AddressPicker").classList.add("d-none");
+    
+  } else {
+    document.getElementById("shipping-address").classList.add("d-none");
+    document.getElementById("shipping-address-readonly").classList.remove("d-none");
+    document.getElementById("AddressPicker").classList.remove("d-none");
+  }
+});
+document.getElementById("billingIsSameAsShipping").querySelector("input").addEventListener('change',(e) => { 
+  store.dispatch({type: "ChangeBillingCheckbox", payload: e.currentTarget.checked});
+  if (e.currentTarget.checked) {
+    document.getElementById("billing-address").classList.add("d-none"); 
+  } else {
+    fillAddress();
+    document.getElementById("billing-address").classList.remove("d-none");
+  }
+});
+document.getElementById("AddressPicker").addEventListener('change',(e) => {  
+  store.dispatch({type: "UpdateDeliveryAddress", payload: parseFloat(e.currentTarget.value)}); 
+  renderDeliveryAddressReadonly(parseFloat(e.currentTarget.value));
+});
+document.getElementById("EcomOrderDeliveryCountry").addEventListener('change', (e) => {
+  getDataJSON(e.currentTarget.value)
+    .then((data)=>{return outputRegions(data, document.getElementById("EcomOrderDeliveryRegion"))})   
+});
+document.getElementById("EcomOrderCustomerCountry").addEventListener('change', (e) => {
+  getDataJSON(e.currentTarget.value)
+    .then((data)=>{return outputRegions(data, document.getElementById("EcomOrderCustomerRegion"))})   
+});
+
+
+
+
+//Init
+getDataJSON()
+  .then((data) => {return featureCountry(data,"CA")})
+  .then((data) => {return featureCountry(data,"US")})
+  .then((data) => {return outputCountries(data)}) 
+  .then((data) => {return outputRegions(data)})
+renderDeliveryAddressReadonly(parseFloat(document.getElementById("AddressPicker").value));  
